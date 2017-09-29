8099cdf455d9710fa8354dc44aec6c3c0f1b4cef
Mekesia Brown <mekesiabrown@gmail.com>
2017-09-29 00:37:19 -0400

    Remove old ad blocker detector file

--- public/etc/a2.js
+++
@@ -1,7 +0,0 @@
-if (window.showAds) {
-    console.log("Twitter feed can be loaded. Yay!");
-} else {
-    // You user uses adbocker, show a popup dialog, alert, etc here
-    $("div.twitterhex div.middle").html("<p>Please pause your Adblocker to see Twitter newsfeed!</p>");
-    $("div.tweetr").html("<p>Please pause your Adblocker to see Twitter newsfeed!</p>");
-}
